39ddf9ee6ec0e4548d20d4afab6f0d852417676b
Lars Kristensen <lmkr@hib.no>
2017-09-09 17:52:16 +0200

    First version of starter code for project

---
+++ src/no/hvl/dat100/prosjekt/GPSComputer.java
@@ -0,0 +1,157 @@
+package no.hvl.dat100.prosjekt;
+
+public class GPSComputer {
+	
+	public GPSComputer(GPSData gpsdata) {
+
+		GPSDataConverter converter = new GPSDataConverter(gpsdata);
+		converter.convert();
+
+		this.times = converter.times;
+		this.latitudes = converter.latitudes;
+		this.longitudes = converter.longitudes;
+		this.elevations = converter.elevations;
+	}
+
+	// tabeller for GPS datapunkter
+	public int[] times;
+	public double[] latitudes;
+	public double[] longitudes;
+	public double[] elevations;
+	
+	// beregn total distances (i meter)
+	public double totalDistance() {
+
+		double distance = 0;
+
+		// TODO
+		// OPPGAVE - START
+
+		// Hint: bruk distance-metoden fra GPSUtils.
+		
+		// OPPGAVE - SLUTT
+
+		return distance;
+	}
+
+	// beregn totale høydemeter (i meter)
+	public double totalElevation() {
+
+		double elevation = 0;
+
+		// TODO
+		// OPPGAVE - START
+
+		// OPPGAVE - SLUTT
+		return elevation;
+	}
+
+	// beregn total tiden for hele turen (i sekunder)
+	public int totalTime() {
+		
+		int totaltime = 0;
+		
+		// TODO 
+		// OPPGAVE START
+		
+		// OPPGAVE SLUTT
+		
+		return totaltime;
+	}
+		
+	// beregn gjennomsnitshastighets mellom hver av gps punktene
+	public double[] speeds() {
+
+		double[] speeds = new double[times.length-1];
+		
+		// TODO
+		// OPPGAVE - START
+		
+		// OPPGAVE - SLUTT
+		return speeds;
+	}
+
+	// beregn maximum hastighet km/t
+	public double maxSpeed() {
+		
+		double maxspeed = 0;
+		
+		// TODO
+		// OPPGAVE - START
+				
+		// OPPGAVE - SLUTT
+		
+		return maxspeed;
+	}
+	
+	// beregn gjennomsnittshasitiget for hele turen km/t
+	public double averageSpeed() {
+
+		double average = 0;
+		
+		// TODO
+		// OPPGAVE - START
+				
+		// OPPGAVE - SLUTT
+		
+		return average;
+	}
+
+
+	// conversion factor m/s to miles per hour
+	public static double MS = 2.236936;
+
+	// beregn kcal gitt weight og tid der kjøres med en gitt hastighet
+	public double kcal(double weight, int secs, double speed) {
+
+		double kcal = 0;
+
+		// MET: Metabolic equivalent of task angir (kcal x kg-1 x h-1)
+		double met = 0;		
+		double speedmph = speed * MS;
+
+		// TODO
+		// OPPGAVE START
+		
+		// Energy Expended (kcal) = MET x Body Weight (kg) x Time (h)
+
+		// OPPGAVE SLUTT
+		
+		return kcal;
+	}
+
+	public double totalKcal(double weight) {
+
+		double totalkcal = 0;
+
+		// TODO
+		// OPPGAVE - START 
+		
+		// Hint: hent hastigheter i speeds tabellen og tider i timestabellen
+		// disse er definer i toppen av klassen og lese automatisk inn
+		
+		// OPPGAVE - SLUTT
+		
+		return totalkcal;
+	}
+	
+	private static double WEIGHT = 80.0;
+	
+	// skriv ut statistikk for turen
+	public void print() {
+		
+		// TODO
+		// OPPGAVE - START
+				
+		// OPPGAVE - SLUT
+	}
+	
+	// ekstraoppgaver
+	public void climbs() {
+		
+	}
+	
+	public void maxClimb() {
+		
+	}
+}
---
+++ src/no/hvl/dat100/prosjekt/GPSUtils.java
@@ -0,0 +1,98 @@
+package no.hvl.dat100.prosjekt;
+
+import static java.lang.Math.*;
+
+public class GPSUtils {
+
+	public GPSUtils() {
+	
+	}
+	
+	// konverter sekunder til string på formen hh:mm:ss
+	public static String printTime(int secs) {
+		
+		String timestr = "";
+		String TIMESEP = ":";
+		
+		// TODO
+		// OPPGAVE - START
+				
+		// OPPGAVE - SLUTT
+		
+		return timestr;
+	}
+	
+	// beregn maximum av en tabell av doubles med minnst et element
+	public static double findMax(double[] da) {
+
+		double max = da[0];
+
+		for (double d : da) {
+			if (d > max) {
+				max = d;
+			}
+		}
+
+		return max;
+	}
+
+	// beregn minimum av en tabell av doubles med minnst et element
+	public static double findMin(double[] da) {
+
+		// fjern = "0.0" når metoden implementeres for ikke få forkert minimum
+		double min = 0.0; 
+
+		// TODO
+		// OPPGAVE - START
+
+		// OPPGAVE - SLUT
+		return min;
+	}
+
+	
+	private static int R = 6371000; // jordens radius
+	
+	// Beregn avstand mellom to gps punkter ved bruk av Haversine formlen
+	public static double distance(double latitude1, double longitude1, double latitude2, double longitude2) {
+
+		double a,c,d = 1.0; // fjern = 1.0
+		
+		// TODO:
+		// OPPGAVE - START
+		
+		// OPPGAVE - SLUTT
+
+		return d;
+	}
+	
+	// beregn gjennomsnits hastighet i km/t mellom to gps punkter
+	public static double speed(int secs, double latitude1, double longitude1, double latitude2, double longitude2) {
+
+		double speed = 0.0;
+
+		// TODO:
+		// OPPGAVE - START
+		
+		// OPPGAVE - SLUTT
+
+		return speed;
+	}
+	
+	private static int TEXTWIDTH = 10;
+	
+	// konverter double til string med 2 decimaler og streng lengde 10
+	// eks. 1.346 konverteres til "      1.35" og enhet til slutt
+	// Hint: se på String.format metoden
+	
+	public static String printDouble(double d) {
+		
+		String str = "";
+		
+		// TODO
+		// OPPGAVE - START
+		
+		// OPPGAVE - SLUTT
+		
+		return str;
+	}
+}
